c77aaf95c5f5a7c04e3676d60cb11b22fd8f381c
yasiqb89 <mian.yassir@gmail.com>
2025-04-06 22:33:32 +0200

    test login, test invalid login

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import Page
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.username_input = page.locator("[data-test=\"username\"]")
+        self.password_input = page.locator("[data-test=\"password\"]")
+        self.login_button = page.locator("[data-test=\"login-button\"]")
+        self.error_message = page.locator("[data-test='error']")
+
+    def navigation(self):
+        self.page.goto("https://www.saucedemo.com/")
+    
+    def login(self, username: str, password: str):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+    def get_error_text(self):
+        return self.error_message.inner_text()
+    
+    
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,20 @@
-import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+from pages.login_page import LoginPage
 
 
-def test_valid_login(page):
-    page.goto("https://www.saucedemo.com/")
-    page.fill('input[data-test="username"]', 'standard_user')
-    page.fill('input[data-test="password"]', 'secret_sauce')
-    page.click('input[data-test="login-button"]')
+def test_valid_login(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigation()
+    login_page.login("standard_user", "secret_sauce")
 
+    # Validate login access using text and url validation
+    expect(page.locator("[data-test=\"primary-header\"]")).to_contain_text("Swag Labs")
     assert page.url == "https://www.saucedemo.com/inventory.html"
+
+def test_invalid_login(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigation()
+    login_page.login("invalid_user", "wrong_password")
+
+    expect(login_page.error_message).to_be_visible()
+    expect(login_page.error_message).to_have_text("Epic sadface: Username and password do not match any user in this service")
