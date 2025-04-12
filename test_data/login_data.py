a2a390c2079e58d85b9161454102533fa25e5330
yasiqb89 <mian.yassir@gmail.com>
2025-04-12 15:15:04 +0200

    added login data

---
+++ test_data/login_data.py
@@ -0,0 +1,21 @@
+
+
+valid_credentials = {
+    "username": "standard_user",
+    "password": "secret_sauce"
+}
+
+
+invalid_credentials = [
+    {"username": "", "password": "", "error": "Epic sadface: Username is required"},
+    {"username": "standard_user", "password": "", "error": "Epic sadface: Password is required"},
+    {"username": "invalid_user", "password": "wrong", "error": "Epic sadface: Username and password do not match any user in this service"}
+]
+
+
+locked_out_user = {
+    "username": "locked_out_user",
+    "password": "secret_sauce",
+    "error": "Epic sadface: Sorry, this user has been locked out."
+}
+
